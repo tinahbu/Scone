3ca7d915adc6993fca8035456896dc2503298e31
Tina Hong Bu <butianhong@outlook.com>
2017-05-31 11:34:47 -0400

    Add testing code

--- MacbookKB.lisp
+++ MacbookKB.lisp
@@ -37,3 +37,27 @@
 (x-is-the-y-of-z {operating system of Macbook_2} {operating system of computer} {Macbook_2})
 (new-is-a {operating system of Macbook_2} {MacOS}) ;;; change parent to {MacOS}
 (x-is-the-y-of-z (new-string {"10.9.1"}) {version of software} {operating system of Macbook_2})
+
+;;; Testing
+
+;;; extracts the value from the iname 先別用這個
+;;(node-value {version of MacOS})
+;;; can-x-be-the-y-of-z? returns T or NIL (unhappy split or constraint)
+(can-x-be-the-y-of-z? {MacOS} {operating system of computer} {Macbook}) ;; T
+(can-x-be-the-y-of-z? {Linux} {operating system of computer} {Macbook}) ;; NIL
+(can-x-be-the-y-of-z? {Windows} {operating system of computer} {Macbook}) ;; NIL
+(is-x-a-y? {operating system of computer} {operating system}) ;;; YES
+;;; the-x-of-y, query a equal relationship or a proper node?
+(the-x-of-y {operating system of computer} {Macbook}) ;;; {operating system of Macbook}
+(the-x-of-y {operating system of computer} {computer}) ;;; NIL
+
+(is-x-a-y? {operating system of Macbook} {Linux}) ;;; NO
+(is-x-a-y? {operating system of Macbook} {MacOS}) ;;; YES
+(is-x-a-y? {operating system of Macbook} {Windows}) ;;; NO
+
+;;; query individual node's OS version number
+(the-x-of-y {version of software} {operating system of Macbook_1}) ;;; 10.9.2
+(the-x-of-y {version of software} {operating system of Macbook_2}) ;;; 10.9.1
+
+(the-x-of-y {operating system of Macbook} {Macbook_1}) ;;; 
+(the-x-of-y {operating system of computer} {Macbook_1}) ;;; {}
